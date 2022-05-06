04772a713eecbe6fccb8a4b1f1d3dad61f015a7c
Luis <Luis@PCLuisM1>
2022-05-06 13:38:16 -0600

    Este es mi intento de subir el proyecto datos a gitHub

---
+++ src/datos_poo/Datos_POO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos_poo;
+
+/**
+ *
+ * @author Luis
+ */
+public class Datos_POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Luis Manuel Sanchez Gonzalez");
+        System.out.println("Ingenieria en SIstemas Computacionales");
+        System.out.println("Programacion Orientada a Objetos");
+        // TODO code application logic here
+    }
+    
+}
